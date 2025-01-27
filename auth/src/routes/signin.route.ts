7dc43dda1d0672a370d1c400cf0c1602a186bed6
badAl <badalsingh120403@gmail.com>
2025-01-27 16:48:02 +0530

    refactor: move request validator middleware to validateRequest.middleware.ts file

--- auth/src/routes/signin.route.ts
+++ auth/src/routes/signin.route.ts
@@ -1,9 +1,24 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validateRequest.middleware';
 
 const router = express.Router();
 
-router.get('/api/users/singin', (req, res) => {
-    res.send('Hello');
-});
+router.post(
+    '/api/users/signin',
+    [
+        body('email').isEmail().withMessage('Email must be valid'),
+        body('password')
+            .trim()
+            .notEmpty()
+            .withMessage('Password must be provided'),
+    ],
+    validateRequest,
+    (req: Request, res: Response) => {
+        const { email, password } = req.body;
+
+        res.send({});
+    },
+);
 
 export { router as siginRouter };
--- auth/src/routes/signup.route.ts
+++ auth/src/routes/signup.route.ts
@@ -1,10 +1,10 @@
 import express, { Request, Response } from 'express';
-import { validationResult, body, Result } from 'express-validator';
+import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
 
-import { RequestValidationError } from '../errors/requestValidation.error';
 import { BadRequestError } from '../errors/badRequest.error';
 import { User } from '../models/user.model';
+import { validateRequest } from '../middlewares/validateRequest.middleware';
 
 const router = express.Router();
 
@@ -17,13 +17,8 @@ router.post(
             .isLength({ min: 4, max: 20 })
             .withMessage('Password must be between 4 and 20 character'),
     ],
+    validateRequest,
     async (req: Request, res: Response) => {
-        const errors: Result = validationResult(req);
-
-        if (!errors.isEmpty()) {
-            throw new RequestValidationError(errors.array());
-        }
-
         const { email, password } = req.body;
 
         const existingUser = await User.findOne({ email });
