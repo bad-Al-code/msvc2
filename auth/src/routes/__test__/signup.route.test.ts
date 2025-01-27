4f729046702ae2d7f5d59ff96a1730751249bdb4
badAl <badalsingh120403@gmail.com>
2025-01-27 23:21:57 +0530

    test: write some response test for signup route

--- auth/src/routes/__test__/signup.route.test.ts
+++ auth/src/routes/__test__/signup.route.test.ts
@@ -1,13 +1,49 @@
-import { it } from 'vitest';
+import { describe, it } from 'vitest';
 import request from 'supertest';
 import { app } from '../../app';
 
-it('it returns a 201 on successful signup', async () => {
-    return request(app)
-        .post('/api/users/signup')
-        .send({
-            email: 'al@al.com',
-            password: '1234',
-        })
-        .expect(201);
+describe('Signup Route', () => {
+    it('it returns a 201 on successful signup', async () => {
+        return request(app)
+            .post('/api/users/signup')
+            .send({
+                email: 'al@al.com',
+                password: '1235',
+            })
+            .expect(201);
+    });
+
+    it('returns a 400 with an invalid email', async () => {
+        return request(app)
+            .post('/api/users/signup')
+            .send({
+                email: '214',
+                password: '123214',
+            })
+            .expect(400);
+    });
+
+    it('returns a 400 with an invalid password', async () => {
+        return request(app)
+            .post('/api/users/signup')
+            .send({
+                email: 'al@al.com',
+                password: '12',
+            })
+            .expect(400);
+    });
+
+    it('returns a 400 with missing email and password', async () => {
+        await request(app)
+            .post('/api/users/signup')
+            .send({ email: 'al@al.com' })
+            .expect(400);
+
+        await request(app)
+            .post('/api/users/signup')
+            .send({
+                password: '1234',
+            })
+            .expect(400);
+    });
 });
