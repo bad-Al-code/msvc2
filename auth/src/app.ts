b6d32a65759218c24bf78436a8f1eec2b401a2cd
badAl <badalsingh120403@gmail.com>
2025-01-27 15:40:40 +0530

    feat: set jwt as cookie on session using cookie-session npm package

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/currentUser.route';
 import { siginRouter } from './routes/signin.route';
@@ -8,8 +9,15 @@ import { signupRouter } from './routes/signup.route';
 import { errorHandler } from './middlewares/errorHandler.middleware';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true,
+    }),
+);
 
 app.use(currentUserRouter);
 app.use(siginRouter);
--- auth/src/routes/signup.route.ts
+++ auth/src/routes/signup.route.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express';
 import { validationResult, body, Result } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import { RequestValidationError } from '../errors/requestValidation.error';
-import { User } from '../models/user.model';
 import { BadRequestError } from '../errors/badRequest.error';
+import { User } from '../models/user.model';
 
 const router = express.Router();
 
@@ -29,9 +31,12 @@ router.post(
             throw new BadRequestError('Email in use');
         }
 
-        // TODO: hash the password
         const user = new User({ email, password });
         await user.save();
+
+        const userJwt = jwt.sign({ id: user.id, email: user.email }, 'asdf');
+
+        req.session = { jwt: userJwt };
 
         res.status(201).send(user);
     },
