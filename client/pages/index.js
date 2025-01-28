6ba4d03e846ae21215a4a33260dc370d5182a08a
badAl <badalsingh120403@gmail.com>
2025-01-28 15:36:20 +0530

    feat: get to the currentuser api through ingress  nginx service and pass the cookie to auth service

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,29 @@
-export default function Page() {
+import axios from 'axios';
+
+const Page = ({ currentUser }) => {
+    console.log(currentUser);
+    // axios.get('/api/users/currentUser');
+
     return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+};
+Page.getInitialProps = async ({ req }) => {
+    if (typeof window === 'undefined') {
+        const { data } = await axios.get(
+            /** <service-name>.<namespace>.svc.cluster.local */
+            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentUser',
+            {
+                headers: req.headers,
+            }
+        );
+
+        return data;
+    } else {
+        const response = await axios.get('/api/users/currentUser');
+
+        return response.data;
+    }
+
+    return {};
+};
+
+export default Page;
