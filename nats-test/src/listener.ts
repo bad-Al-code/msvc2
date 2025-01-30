408de1208499c14c5186794a7da612eb089f3ebc
badAl <badalsingh120403@gmail.com>
2025-01-31 01:34:35 +0530

    feat: create a queugroup to limit listeners to listen on channels
    - creat a random listener id so we can avoid listners service error
    - cretate a orderServiceQueueGroup to creat a quoue group

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,17 +1,27 @@
-import nats from 'node-nats-streaming';
+import nats, { Message } from 'node-nats-streaming';
+import crypto from 'node:crypto';
 
 console.clear();
 
-const stan = nats.connect('ticketing', '124', {
+const stan = nats.connect('ticketing', crypto.randomUUID(), {
     url: 'http://localhost:4222',
 });
 
 stan.on('connect', () => {
     console.log('NATS listener');
 
-    const subscription = stan.subscribe('ticket:created');
+    const subscription = stan.subscribe(
+        'ticket:created',
+        'ordersServiceQueueGroup',
+    );
 
-    subscription.on('message', (msg) => {
-        console.log('message received');
+    subscription.on('message', (msg: Message) => {
+        const data = msg.getData();
+
+        if (typeof data === 'string') {
+            console.log(
+                `Reveiced event #${msg.getSequence()}, with data: ${data}`,
+            );
+        }
     });
 });
