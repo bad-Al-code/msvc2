cc25af3769c48bc50c214a6d7994f979b5ea0dd5
badAl <badalsingh120403@gmail.com>
2025-01-31 05:07:21 +0530

    feat: use custom publisher

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -8,13 +8,17 @@ const stan = nats.connect('ticketing', 'abc', {
     url: 'http://localhost:4222',
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
     console.log('NATS Publisher');
 
     const publisher = new TicketCreatedPublisher(stan);
-    publisher.publish({
-        id: 'qwe',
-        title: 'asdadsa',
-        price: 123,
-    });
+    try {
+        await publisher.publish({
+            id: 'qwe',
+            title: 'asdadsa',
+            price: 123,
+        });
+    } catch (err) {
+        console.log(err);
+    }
 });
