800ee26390d6a17421bbbc8dce123f15da309991
badAl <badalsingh120403@gmail.com>
2025-02-06 18:30:13 +0530

    feat(Orders): create router for creating order
    - Validate input
    - make sure user is authenticated
    - Find the ticket the user is trying to order in the database
    - Ticket is not already reserved
    - Calculate expiration date or this order
    - Build the order and save to the database

--- orders/src/routes/new.route.ts
+++ orders/src/routes/new.route.ts
@@ -1,9 +1,65 @@
-import express, { Request, Response } from 'express';
+import { Request, Response, Router } from 'express';
+import {
+    NotFoundError,
+    OrderStatus,
+    requireAuth,
+    validateRequest,
+} from '@badalcodeorg/common';
+import { body, ExpressValidator } from 'express-validator';
+import { Types } from 'mongoose';
 
-const router = express.Router();
+import { Ticket } from '../models/ticket.model';
+import { Order } from '../models/order.model';
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-    res.send({});
-});
+const router = Router();
+
+// const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
+router.post(
+    '/api/orders',
+    requireAuth,
+    [
+        body('ticketId')
+            .not()
+            .isEmpty()
+            .custom((input: string) => Types.ObjectId.isValid(input))
+            .withMessage('TicketId must be provided'),
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => {
+        /**
+         * TODO:
+         * 1. Find the ticket the user is trying to order in the database
+         * 2.Ticket is not already reserved
+         * 3. Calculate expiration date or this order
+         * 4. Build the order and save to the database
+         * 5. Publish event, order:created
+         */
+
+        const { ticketId } = req.body;
+
+        const ticket = await Ticket.findById(ticketId);
+        if (!ticket) {
+            throw new NotFoundError();
+        }
+
+        const expiration = new Date();
+        expiration.setSeconds(
+            expiration.getSeconds() +
+                Number(process.env.EXPIRATION_WINDOW_SECONDS),
+        );
+
+        const order = Order.build({
+            userId: req.currentUser!.id,
+            status: OrderStatus.Created,
+            expiresAt: expiration,
+            ticket,
+        });
+
+        await order.save();
+
+        res.status(201).send({ order });
+    },
+);
 
 export { router as newOrderRouter };
