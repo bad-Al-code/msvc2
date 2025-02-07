cf341db9be952fc08976ab4b3bdf1b74f6db1393
badAl <badalsingh120403@gmail.com>
2025-02-07 18:51:32 +0530

    feat(Orders): Add optimistic concurrency control to Ticket model
    - Installed `mongoose-update-if-current` to enable optimistic
    concurrency control (OCC).
    - Modified `ticket.model.ts` to:
      - Import and apply `updateIfCurrentPlugin` from
    `mongoose-update-if-current`.
      - Set the version key to `version` for tracking document versions.
      - Extend `TicketDoc` to include a `version` field.
    - Add unit test for testing version is working as expected
      - check for occ
      - hcehck for version number incremnet in duplicate save

---
+++ tickets/src/models/__test__/ticket.model.test.ts
@@ -0,0 +1,47 @@
+import { describe, it, expect } from 'vitest';
+import { Ticket } from '../ticket.model';
+
+describe('Ticket Model habdle concurrency ', () => {
+    it('should implement optimistiz concurrency control', async () => {
+        const ticket = Ticket.build({
+            title: 'Title',
+            price: 10,
+            userId: '123',
+        });
+
+        await ticket.save();
+
+        const firstInstance = await Ticket.findById(ticket.id);
+        const secondInstance = await Ticket.findById(ticket.id);
+
+        firstInstance!.set({ price: 15 });
+        secondInstance!.set({ price: 20 });
+
+        await firstInstance!.save();
+
+        expect(async () => {
+            await secondInstance!.save();
+        }).rejects.toThrow();
+    });
+
+    it('should increments multople version number on multople saves', async () => {
+        const ticket = Ticket.build({
+            title: 'Title',
+            price: 10,
+            userId: '123',
+        });
+
+        await ticket.save();
+        console.log(ticket);
+
+        expect(ticket.version).toEqual(0);
+
+        await ticket.save();
+        console.log(ticket);
+        expect(ticket.version).toEqual(1);
+
+        await ticket.save();
+        console.log(ticket);
+        expect(ticket.version).toEqual(2);
+    });
+});
