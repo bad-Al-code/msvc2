d04df40ce807e0e1425726c6af69ce18d4795a18
Badal <badalsingh120403@gmail.com>
2025-02-17 14:47:44 +0530

    chore(expiration): testing job processing

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,16 +1,8 @@
-import mongoose from 'mongoose';
-import http from 'node:http';
-
-import { app } from './app';
 import { natsWrapper } from './natsWrapper';
 
+import { OrderCreatedListener } from './events/listeneres/orderCreated.listener';
+
 const start = async () => {
-    if (!process.env.JWT_KEY) {
-        throw new Error('JWT_KEY is not provided');
-    }
-    if (!process.env.MONGO_URI) {
-        throw new Error('MONGO_URI is not provided');
-    }
     if (!process.env.NATS_CLIENT_ID) {
         throw new Error('NATS_CLIENT_ID is not provided');
     }
@@ -20,6 +12,7 @@ const start = async () => {
     if (!process.env.NATS_URL) {
         throw new Error('NATS_URL is not provided');
     }
+
     try {
         await natsWrapper.connect(
             process.env.NATS_CLUSTER_ID,
@@ -35,17 +28,10 @@ const start = async () => {
         process.on('SIGINT', () => natsWrapper.client.close());
         process.on('SIGTERM', () => natsWrapper.client.close());
 
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('Connected to DB');
+        new OrderCreatedListener(natsWrapper.client).listen();
     } catch (error) {
-        console.error('Error connecting to DB:', error);
+        console.log(error);
     }
-
-    const server = http.createServer(app);
-
-    server.listen(3000, () => {
-        console.log('Tickets service running on port 3000!');
-    });
 };
 
 start();
